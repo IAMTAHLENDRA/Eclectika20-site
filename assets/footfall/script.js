d19b589d24f5dade54cde7379531d20301be0dea
Corruptthreat <drawte786@gmail.com>
2020-02-13 14:29:24 +0530

    footfall

---
+++ assets/footfall/script.js
@@ -0,0 +1,18 @@
+(function($) {
+		"use strict";
+		function count($this){
+		var current = parseInt($this.html(), 10);
+		current = current + 1; /* Where 50 is increment */	
+		$this.html(++current);
+			if(current > $this.data('count')){
+				$this.html($this.data('count'));
+			} else {    
+				setTimeout(function(){count($this)}, 0.1);
+			}
+		}        	
+		$(".stat-count").each(function() {
+		  $(this).data('count', parseInt($(this).html(), 10));
+		  $(this).html('0');
+		  count($(this));
+		});
+   })(jQuery);
